8efb5cf93ba75958c783b7c56337bd2872877247
Bewizi <toluwalase.bewaji@yahoo.com>
2024-01-16 11:20:29 +0100

    bewizi

--- app.js
+++ app.js
@@ -25,3 +25,35 @@ window.addEventListener("resize", function () {
 
 // Trigger the resize event on page load
 window.dispatchEvent(new Event("resize"));
+
+// form validation
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+// form id
+let fullname = id("fullname"),
+  email = id("email"),
+  form = id("form"),
+  message = id("text");
+
+// form class
+errorMsg = classes("error");
+
+// btn event
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(fullname, 0, "fullname cannot be blank");
+  engine(email, 1, "email cannot be blank");
+  engine(message, 2, "message cannot be blank");
+});
+
+// create a function named engine which will do all sorts of form validation work for us
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() == "") {
+    errorMsg[serial].innerHTML = message;
+  } else {
+    errorMsg[serial].innerHTML = " ";
+  }
+};
